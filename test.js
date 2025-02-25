339369a2c7a03ea0fe53beffea6e0e920d46de3d
Anaditya0 <andysaini1303@gmail.com>
2025-02-25 18:04:58 +0530

    How to deal with authentication error

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 Test
+
+If the authentication error comes in GIT while uploading a file or pulling a file.
+
+We need to genrate personal access tokens.
+	Go into GIT SETTINGS > DEVELOPER SETTINGS > PERSONAL ACCESS TOKENS
+
+After genrating the token copy and paste it somewhere as we can see it only once.
+
+Then come back to your command line 
+	first see what is the repo your GIT is pushing code to - //git remote -v
+	then push copy the https link from GIT and push this code in CLI - git remote set-url origin https:/token@github.com/andrestofURL.
